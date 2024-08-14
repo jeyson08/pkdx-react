96453962967f416efbdf02812b615aeb1fa86027
jeyson08 <jeyson.boursault@gmail.com>
2024-08-14 18:14:39 +0200

    customeId

--- src/components/TableLine.js
+++ src/components/TableLine.js
@@ -10,9 +10,20 @@ const TableLine = ({ pkm }) => {
     }
   }, [pkm]);
 
+  const customId = (id) => {
+    if (id < 10) {
+      return "#00" + id;
+    } else if (id >= 10 && id < 100) {
+      return "#0" + id;
+    } else {
+      return "#" + id;
+    }
+  };
+
   return (
     <div className="table-line">
-      <h1>{pkmSoloData && pkmSoloData.name}</h1>
+      <p>{pkmSoloData && customId(pkmSoloData.id)}</p>
+      <p>{pkmSoloData && pkmSoloData.name}</p>
       <img
         src={pkmSoloData.sprites && pkmSoloData.sprites.front_default}
         alt={pkmSoloData.name}
