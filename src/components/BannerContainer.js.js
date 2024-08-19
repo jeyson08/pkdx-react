fef82e3661df3911292327acc9e8e2edea95a757
jeyson08 <jeyson.boursault@gmail.com>
2024-08-19 14:51:25 +0200

    animation

---
+++ src/components/BannerContainer.js.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const BannerContainer = () => {
+  const [randomPkm, setRandomPkm] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const uniqueNumbers = new Set();
+
+      // Générer 6 nombres aléatoires uniques
+      while (uniqueNumbers.size < 13) {
+        const randomNumber = Math.floor(Math.random() * 700) + 1;
+        uniqueNumbers.add(randomNumber);
+      }
+
+      const promises = Array.from(uniqueNumbers).map((number) =>
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${number}/`)
+      );
+
+      try {
+        const results = await Promise.all(promises);
+        const pokemons = results.map((res) => res.data);
+        setRandomPkm(pokemons);
+      } catch (error) {
+        console.error("Error fetching Pokémon:", error);
+      }
+    };
+
+    fetchPokemons();
+  }, []);
+
+  return (
+    <div className="banner-container">
+      {randomPkm.map((pokemon, index) => (
+        <div key={index}>
+          <img
+            src={pokemon.sprites.other["official-artwork"].front_default}
+            alt={pokemon.name}
+            title={pokemon.name}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BannerContainer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
+import BannerContainer from "./BannerContainer.js";
 
 const Header = () => {
   return (
     <div className="header-container">
+      <div className="animation"></div>
       <header>
         <h1>Pokédex</h1>
       </header>
+      <BannerContainer />
     </div>
   );
 };
