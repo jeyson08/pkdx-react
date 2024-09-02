34a5bcfbc3d3d48d8a3096cce8133f00966d7bb9
jeyson08 <jeyson.boursault@gmail.com>
2024-09-02 09:54:58 +0200

    stylecard css

--- src/components/StyleCard.js
+++ src/components/StyleCard.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { customId } from "./Utils";
+import { NavLink } from "react-router-dom";
 
 const StyleCard = ({ url, id }) => {
   const [pkmData, setPkmData] = useState([]);
@@ -11,26 +12,28 @@ const StyleCard = ({ url, id }) => {
 
   return (
     <div className="style-card">
-      <div className="img-container">
-        {pkmData.sprites && (
-          <img
-            src={pkmData.sprites.other["official-artwork"].front_default}
-            alt={pkmData.name}
-          />
-        )}
-      </div>
-      <div className="style-info">
-        {id && <p className="id">{customId(id)}</p>}
-        {pkmData.name && <p className="name">{pkmData.name}</p>}
-      </div>
-      <ul className="types">
-        {pkmData.types &&
-          pkmData.types.map((type) => (
-            <li className={type.type.name} key={type.type.name}>
-              {type.type.name}
-            </li>
-          ))}
-      </ul>
+      <NavLink to={`/pokemon/${pkmData.name}`} state={{ pokemonData: pkmData }}>
+        <div className="img-container">
+          {pkmData.sprites && (
+            <img
+              src={pkmData.sprites.other["official-artwork"].front_default}
+              alt={pkmData.name}
+            />
+          )}
+        </div>
+        <div className="style-info">
+          {id && <p className="id">{customId(id)}</p>}
+          {pkmData.name && <p className="name">{pkmData.name}</p>}
+        </div>
+        <ul className="types">
+          {pkmData.types &&
+            pkmData.types.map((type) => (
+              <li className={type.type.name} key={type.type.name}>
+                {type.type.name}
+              </li>
+            ))}
+        </ul>
+      </NavLink>
     </div>
   );
 };
